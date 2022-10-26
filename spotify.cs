5fa98301c0031b3e1b94db3fd1374194ca7f9e11
thegodhimself119 <97171793+thegodhimself119@users.noreply.github.com>
2022-10-26 23:45:26 +0545

    fixed link bug

--- spotify.cs
+++ spotify.cs
@@ -2,102 +2,119 @@
 using RestSharp;
 using System.Diagnostics;
 
-namespace spotifylibrary
-{
-    public class spotify
-    {
+namespace spotifylibrary;
 
-        public static void SearchYoutube(String SongName,String Directory)
+public class spotify
+{
+
+    public static void SearchYoutube(String SongName,String Directory)
+    {
+        try
         {
             var pro = new Process
             {
                 StartInfo =
-                {
-                    FileName = @"C:\\Windows\\system32\\cmd.exe",
-                    WorkingDirectory =@$"{Directory}",
-                    Arguments = $"/C yt-dlp.exe ytsearch:{SongName} --no-playlist --extract-audio --audio-format mp3",
-                    CreateNoWindow = true
+            {
+                FileName = @"C:\\Windows\\system32\\cmd.exe",
+                WorkingDirectory =@$"{Directory}",
+                Arguments = $"/C yt-dlp.exe ytsearch:{SongName} --no-playlist --extract-audio --audio-format mp3",
+                CreateNoWindow = true
 
 
-                }
+            }
 
             };
 
             pro.Start();
-
-            //alternate 
-            /* System.Diagnostics.Process process = new System.Diagnostics.Process();
-             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-             startInfo.FileName = "cmd.exe";
-            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-             startInfo.WorkingDirectory = @"F:\ytdlp\";
-             startInfo.Arguments = "/C yt-dlp.exe ytsearch:hello --no-playlist --extract-audio --audio-format mp3";
-             process.StartInfo = startInfo;
-             process.Start();*/
         }
-
-
-        public static String ProcessUrl(String url)
+        catch (System.IO.IOException e)
         {
-
-            string spotlink = "https://open.spotify.com/playlist/";
-            if (url.Contains(spotlink) == true)
-            {
-
-                String processed = url.Substring(url.IndexOf(spotlink) + spotlink.Length, url.Length - spotlink.Length);
-                return processed;
-            }
-
-            else
-            {
-                return "invalid playlist link";
-            }
+            Console.WriteLine("couldnt download");
         }
 
-        public static void GetFromSpotify(String Url, String ApiKey, String DirectoryOfYt)
-        {
-            String newUrl = ProcessUrl(Url);
-            String songname = "";
-            var client = new RestClient($"https://api.apilayer.com/spotify/playlist_tracks?id={newUrl}");
-
-            var request = new RestRequest();
-
-            request.AddHeader("apikey", $"{ApiKey}");
-            RestResponse response = client.Execute(request);
-
-
-            
-
-            JObject obj = JObject.Parse(response.Content);
-            var name = obj["name"];
-            var image = obj["images"][0]["url"];
-
-            JArray title = (JArray)obj["tracks"]["items"];
-            for (int i = 0; i < title.Count; i++)
-            {
-                songname = "";
-                var trackName = title[i]["track"]["name"];
-                JArray artists = (JArray)title[i]["track"]["album"]["artists"];
-
-                for (int k = 0; k < artists.Count; k++)
-                {
-
-                    songname = songname + trackName + " " + artists[k]["name"];
-
-                }
-                SearchYoutube(songname,DirectoryOfYt);
-
-
-
-            }
-        }
-
-
+        //alternate 
+        /* System.Diagnostics.Process process = new System.Diagnostics.Process();
+         System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+         startInfo.FileName = "cmd.exe";
+        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+         startInfo.WorkingDirectory = @"F:\ytdlp\";
+         startInfo.Arguments = "/C yt-dlp.exe ytsearch:hello --no-playlist --extract-audio --audio-format mp3";
+         process.StartInfo = startInfo;
+         process.Start();*/
     }
+
+
+    public static String ProcessUrl(String url)
+    {
+
+        string spotlink = "https://open.spotify.com/playlist/";
+        if (url.Contains(spotlink) == true && url.Contains("?"))
+        {
+
+            String processed = url.Substring(url.IndexOf(spotlink) + spotlink.Length, url.Length - spotlink.Length);
+            String processed2 = processed.Substring(0,processed.IndexOf("?"));
+            return processed2;
+        }
+
+        else if (url.Contains(spotlink) == true)
+        {
+            String processed = url.Substring(url.IndexOf(spotlink) + spotlink.Length, url.Length - spotlink.Length);
+            return processed;
+        }
+
+        else
+        {
+            Console.WriteLine("invalid link");
+            return "";
+        }
+    }
+
+    public static void GetFromSpotify(String Url, String ApiKey, String DirectoryOfYt)
+    {
+        
+        String songname = "";
+        var client = new RestClient($"https://api.apilayer.com/spotify/playlist_tracks?id={Url}");
+
+        var request = new RestRequest();
+
+        request.AddHeader("apikey", $"{ApiKey}");
+        RestResponse response = client.Execute(request);
+
+
+        
+
+        JObject obj = JObject.Parse(response.Content);
+        int total =(int) obj["total"];
+        if (total > 99) 
+        {
+            total = 100;
+        }
+        
+        for (int i = 0; i <total ; i++)
+        {
+            var title = obj["items"][i]["track"];
+            songname = "";
+
+            var trackName = title["name"];
+            JArray artists = (JArray)title["album"]["artists"];
+            for (int k = 0; k < artists.Count; k++)
+            {
+
+                songname = songname + trackName + " " + artists[k]["name"];
+
+            }
+            SearchYoutube(songname,DirectoryOfYt);
+        
+
+
+        }
+    }
+
 
 }
 
- 
 
 
-    
+
+
+
---
+++ youtube.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace spotifylibrary
+{
+    internal class youtube
+    {
+        public static void DownloadSong(String songname,String Directory)
+        {
+            var pro = new Process
+            {
+                StartInfo =
+                {
+                    FileName = @"C:\\Windows\\system32\\cmd.exe",
+                    WorkingDirectory =@$"{Directory}",
+                    Arguments = $"/C yt-dlp.exe ytsearch:{songname} --no-playlist --extract-audio --audio-format mp3",
+                    CreateNoWindow = true
+
+
+                }
+
+            };
+
+            pro.Start();
+        }
+
+        public static void init()
+        {
+
+        }
+
+    }
+}
